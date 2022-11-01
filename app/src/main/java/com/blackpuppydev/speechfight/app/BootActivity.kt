8e7447cc099e833c5392ae613189f2bcd62c0f71
blackpuppydev <blackpuppy.dev@gmail.com>
2022-11-01 13:34:31 +0700

    add firebase in project

--- app/src/main/java/com/blackpuppydev/speechfight/app/BootActivity.kt
+++ app/src/main/java/com/blackpuppydev/speechfight/app/BootActivity.kt
@@ -1,13 +1,12 @@
-package com.blackpuppydev.speechfight
+package com.blackpuppydev.speechfight.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blackpuppydev.speechfight.R
 
-class MainActivity : AppCompatActivity() {
+class BootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //test
+        setContentView(R.layout.activity_boot)
     }
 }
