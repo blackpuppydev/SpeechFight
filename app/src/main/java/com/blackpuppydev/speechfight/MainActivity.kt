a18d6111835c8fc40103c46d80d24183c4bf7def
Shopper's MacBook <shoppermacbook@Shoppers-MacBook.local>
2022-11-01 12:36:47 +0700

    Initial commit

---
+++ app/src/main/java/com/blackpuppydev/speechfight/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.blackpuppydev.speechfight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
